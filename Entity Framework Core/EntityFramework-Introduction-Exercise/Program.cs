4b33289c1eeeb0188e8a74900a60fbbff36ff3ca
Kristian Ivanov <kriskopk@abv.bg>
2021-10-28 20:46:30 +0300

    2/15

---
+++ Entity Framework Core/EntityFramework-Introduction-Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SoftUni
+{
+    using System;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.Employees Full Information
+
+
+        }
+    }
+}
+
