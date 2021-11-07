fa92ef1179f40e1e8a7099c230a37fd731e37f6f
Kristian Ivanov <kriskopk@abv.bg>
2021-11-07 21:20:38 +0200

    project

---
+++ Entity Framework Core/Advanced Querying-Exercise/BookShop/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+
+        }
+
+    }
+}
