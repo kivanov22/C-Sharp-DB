617189b682fefd0fa8c23223e98806381b3a4557
Kristian Ivanov <kriskopk@abv.bg>
2021-11-17 16:05:14 +0200

    CarDealer

---
+++ Entity Framework Core/JSON-Processing-Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.DTO;
+    using CarDealer.Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        private static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+
+            //Console.WriteLine(ImportSuppliers(context));
+        }
+
+        //Query 8. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IEnumerable<SupplierDto> suppliers = JsonConvert.DeserializeObject<IEnumerable<SupplierDto>>(inputJson);
+
+            InitializeMapper();
+
+            var mappedSuppliers = mapper.Map<IEnumerable<Supplier>>(suppliers);
+
+            context.AddRange(mappedSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedSuppliers.Count()}.";
+        }
+
+
+        private static void InitializeMapper()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+
+            mapper = new Mapper(mapperConfiguration);
+        }
+    }
+}
