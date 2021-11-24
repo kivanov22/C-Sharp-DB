1aa3465f97aca71dd2af8f73b8ccef128c7b58be
Kristian Ivanov <kriskopk@abv.bg>
2021-11-24 18:27:20 +0200

    Code first database task done

---
+++ Entity Framework Core/Exam-Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+            this.Purchases = new HashSet<Purchase>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+       // [MaxLength(19)]
+        public string Number { get; set; }
+
+        [Required]
+        public string Cvc { get; set; }
+
+        public CardType Type { get; set; }//required by default enum
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exam-Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/Enums/CardType.cs
@@ -0,0 +1,10 @@
+﻿namespace VaporStore.Data.Models.Enums
+{
+    public enum CardType
+    {
+        Debit = 0,
+        Credit = 1,
+        //Debit = 1 ,
+        //Credit = 2
+    }
+}
